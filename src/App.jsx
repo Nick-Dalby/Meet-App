124f973475870847fc78cab63135f0dbdb6e6f34
Nick Dalby <nckdlby@gmail.com>
2022-05-24 12:10:43 +0200

    events & location states

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import './App.css'
+import './nprogress.css'
+
 import EventList from './EventList'
 import CitySearch from './CitySearch'
 import NumberOfEvents from './NumberOfEvents'
+import { extractLocations, getEvents } from './api'
 
-function App() {
-  return (
-    <div className="App">
-      <CitySearch />
-      <EventList />
-      <NumberOfEvents />
-    </div>
-  )
+export class App extends Component {
+  state = { events: [], locations: [] }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents =
+        location === 'all'
+          ? events
+          : events.filter((event) => event.location === location)
+      this.setState({ events: locationEvents })
+    })
+  }
+
+  componentDidMount() {
+    this.mounted = true
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) })
+      }
+    })
+  }
+
+  componentWillUnmount() {
+    this.mounted = false
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+        />
+        <EventList events={this.state.events} />
+        <NumberOfEvents />
+      </div>
+    )
+  }
 }
 
 export default App
