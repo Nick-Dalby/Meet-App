9e20525ac8f60459201bb4e0121a4505f61cb997
Nick Dalby <nckdlby@gmail.com>
2022-05-25 10:24:12 +0200

    dateTime format, event description

--- src/Event.jsx
+++ src/Event.jsx
@@ -10,20 +10,37 @@ export class Event extends Component {
   }
 
   render() {
+    const { event } = this.props
 
-    const { event } = this.props;
+    // format datetime to calendar format
+    const date = new Date(event.start.dateTime)
+    const options = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true,
+    }
+    const formattedDate = date.toLocaleString('en-US', options)
 
     return (
-      <div className='event'>
+      <div className="event">
         <h3 className="event-summary">{event.summary}</h3>
-        <h3 className="event-date">{event.start.dateTime}</h3>
-        <button className="toggle-event-details" onClick={this.handleClick}>Show/hide details</button>
-        {
-          !this.state.collapsed ?
-          <div className="event-details">more TBD event details go here</div>
-          :
-          null
-        }
+        <div className='date-location'>
+        <h5 className="event-date">{formattedDate}</h5>
+        <h5>{event.location}</h5>
+        </div>
+        <button className="toggle-event-details details-btn" onClick={this.handleClick}>
+          Show/hide details
+        </button>
+        {!this.state.collapsed ? (
+          <div className="event-details">
+            <p>
+              {event.description}
+              </p>
+              </div>
+        ) : null}
       </div>
     )
   }
