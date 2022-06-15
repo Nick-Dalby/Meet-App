317c342e641a252fd5fefcab49fd722d1a77e737
Nick Dalby <nckdlby@gmail.com>
2022-06-15 15:19:11 +0200

    showing welcome screen

--- src/App.js
+++ src/App.js
@@ -55,38 +55,21 @@ export class App extends Component {
     return data
   }
 
-  // async componentDidMount() {
-  //   this.mounted = true
-
-  //   const accessToken = localStorage.getItem('access_token')
-  //   const isTokenValid = (await checkToken(accessToken)).error ? false : true
-  //   const searchParams = new URLSearchParams(window.location.search)
-  //   const code = searchParams.get('code')
-  //   this.setState({ showWelcomeScreen: !(code || isTokenValid) })
-  //   if ((code || isTokenValid) && this.mounted) {
-  //     getEvents().then((events) => {
-  //       if (this.mounted) {
-  //         this.setState({ events, locations: extractLocations(events) })
-  //       }
-  //     })
-  //   }
-
-  //   if (!navigator.onLine) {
-  //     this.setState({
-  //       warningText: 'You are offline - displaying cached event data',
-  //     })
-  //   } else {
-  //     this.setState({ warningText: '' })
-  //   }
-  // }
-
-  componentDidMount() {
+  async componentDidMount() {
     this.mounted = true
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) })
-      }
-    })
+
+    const accessToken = localStorage.getItem('access_token')
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true
+    const searchParams = new URLSearchParams(window.location.search)
+    const code = searchParams.get('code')
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) })
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) })
+        }
+      })
+    }
 
     if (!navigator.onLine) {
       this.setState({
@@ -97,13 +80,30 @@ export class App extends Component {
     }
   }
 
+  // componentDidMount() {
+  //   this.mounted = true
+  //   getEvents().then((events) => {
+  //     if (this.mounted) {
+  //       this.setState({ events, locations: extractLocations(events) })
+  //     }
+  //   })
+
+  //   if (!navigator.onLine) {
+  //     this.setState({
+  //       warningText: 'You are offline - displaying cached event data',
+  //     })
+  //   } else {
+  //     this.setState({ warningText: '' })
+  //   }
+  // }
+
   componentWillUnmount() {
     this.mounted = false
   }
 
   render() {
-    // if (this.state.showWelcomeScreen === undefined)
-    //   return <div className="App" />
+    if (this.state.showWelcomeScreen === undefined)
+      return <div className="App" />
 
     return (
       <div className="App">
