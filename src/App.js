8acb347a4e64e35b73f1f77d0a0530eab6042bf2
Nick Dalby <nckdlby@gmail.com>
2022-06-15 14:55:57 +0200

    data visuals with Recharts

---
+++ src/App.js
@@ -0,0 +1,151 @@
+import React, { Component } from 'react'
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts'
+
+import './App.css'
+import './nprogress.css'
+
+import EventGenre from './EventGenre'
+import WelcomeScreen from './WelcomeScreen'
+import EventList from './EventList'
+import CitySearch from './CitySearch'
+import NumberOfEvents from './NumberOfEvents'
+import { extractLocations, getEvents, checkToken, getAccessToken } from './api'
+import { WarningAlert } from './Alert'
+
+export class App extends Component {
+  state = {
+    events: [],
+    locations: [],
+    numberOfEvents: 32,
+    warningText: '',
+    showWelcomeScreen: undefined,
+  }
+
+  handleEventNumberChange = (value) => {
+    this.setState({ numberOfEvents: value })
+  }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents =
+        location === 'all'
+          ? events
+          : events.filter((event) => event.location === location)
+      this.setState({ events: locationEvents })
+    })
+  }
+
+  getData = () => {
+    const { locations, events } = this.state
+    const data = locations.map((location) => {
+      const number = events.filter(
+        (event) => event.location === location
+      ).length
+      const city = location.split(', ').shift()
+      return { city, number }
+    })
+    return data
+  }
+
+  // async componentDidMount() {
+  //   this.mounted = true
+
+  //   const accessToken = localStorage.getItem('access_token')
+  //   const isTokenValid = (await checkToken(accessToken)).error ? false : true
+  //   const searchParams = new URLSearchParams(window.location.search)
+  //   const code = searchParams.get('code')
+  //   this.setState({ showWelcomeScreen: !(code || isTokenValid) })
+  //   if ((code || isTokenValid) && this.mounted) {
+  //     getEvents().then((events) => {
+  //       if (this.mounted) {
+  //         this.setState({ events, locations: extractLocations(events) })
+  //       }
+  //     })
+  //   }
+
+  //   if (!navigator.onLine) {
+  //     this.setState({
+  //       warningText: 'You are offline - displaying cached event data',
+  //     })
+  //   } else {
+  //     this.setState({ warningText: '' })
+  //   }
+  // }
+
+  componentDidMount() {
+    this.mounted = true
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) })
+      }
+    })
+
+    if (!navigator.onLine) {
+      this.setState({
+        warningText: 'You are offline - displaying cached event data',
+      })
+    } else {
+      this.setState({ warningText: '' })
+    }
+  }
+
+  componentWillUnmount() {
+    this.mounted = false
+  }
+
+  render() {
+    // if (this.state.showWelcomeScreen === undefined)
+    //   return <div className="App" />
+
+    return (
+      <div className="App">
+        <h1>Meet-App</h1>
+        <p>Find CareerFoundry meet-ups around the world!<br />
+          Enter your city & filter the number of results...
+        </p>
+        <div className="search-number-inputs">
+          <WarningAlert text={this.state.warningText} />
+          <CitySearch
+            locations={this.state.locations}
+            updateEvents={this.updateEvents}
+          />
+          <NumberOfEvents
+            numberOfEvents={this.state.numberOfEvents}
+            handleEventNumberChange={this.handleEventNumberChange}
+          />
+        </div>
+        <div className="data-vis-wrapper">
+          <EventGenre events={this.state.events} />
+          <ResponsiveContainer height={400}>
+            <ScatterChart margin={{ top: 20, right: 50, bottom: 10, left: 0 }}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="city" type="category" name="city" />
+              <YAxis dataKey="number" type="number" name="number of events" />
+              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+              <Scatter data={this.getData()} fill="#eb5e28" />
+            </ScatterChart>
+          </ResponsiveContainer>
+        </div>
+        <EventList
+          events={this.state.events.slice(0, this.state.numberOfEvents)}
+        />
+        {/* <WelcomeScreen
+          showWelcomeScreen={this.state.showWelcomeScreen}
+          getAccessToken={() => {
+            getAccessToken()
+          }}
+        /> */}
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/EventGenre.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+import { PieChart, Pie, ResponsiveContainer, Cell } from 'recharts';
+
+const EventGenre = ({ events }) => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const getData = () => {
+      const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+      const data = genres.map((genre) => {
+        const value = events.filter((event) => event.summary.split(' ').includes(genre)).length
+        return { name: genre, value };
+      })
+      return data.filter((genre) => genre.value !== 0);
+    }
+    setData(() => getData());
+  },[events])
+  
+  const colors = ["#635f59","#403d39","#252422","#884125","#eb5e28"]
+
+  return (
+    <ResponsiveContainer height={400}>
+      <PieChart width={400} height={400}>
+      <Pie
+            data={data}
+            cx={200}
+            cy={200}
+            labelLine={false}
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="value"
+            label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
+          >
+             {
+      data.map((entry, index) => (
+        <Cell key={`cell-${index}`} fill={colors[index]}/>
+      ))
+    }
+          </Pie>
+      </PieChart>
+
+    </ResponsiveContainer>
+  )
+}
+
+export default EventGenre
